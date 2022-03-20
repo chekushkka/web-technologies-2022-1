7e5be2c00d9925f07f9951493b0ff76c96a26f99
Vika <vika.petrovskaya.02@gmail.com>
2022-03-20 13:09:08 +0500

    2 практика

---
+++ lesson2/lesson2.js
@@ -0,0 +1,20 @@
+const students = [
+    { name: 'Павел', age: 20 },
+    { name: 'Иван', age: 20 },
+    { name: 'Эдем', age: 20 },
+    { name: 'Денис', age: 20 },
+    { name: 'Виктория', age: 20 },
+    { age: 40 },
+]
+function createCounter(students, property)
+{
+    const result =[]
+    for (let i=0;i<students.length;i++){
+        const prov = students[i];
+        if (prov[property]!== undefined) result.push(prov[property]);
+    }
+    return result;
+}
+const result = createCounter(students, 'name')
+
+console.log(result)
